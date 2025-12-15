3d723b57148e1c351ccfed35f310e9838f25d782
Daniel <dt094250@gmail.com>
2025-12-14 23:19:51 -0600

    Actualizacio api.js

--- functions/api.js
+++ functions/api.js
@@ -1,18 +1,14 @@
 import serverless from 'serverless-http';
-import app from '../src/app.js'; 
+import app from '../src/app.js';
 import { SuscripcionModel } from '../src/models/suscripcion.model.js';
 import 'dotenv/config';
 
 try {
-  SuscripcionModel.init().then(() => {
-    console.log("✅ Conexión a BD inicializada para Netlify");
-  }).catch(err => {
+  SuscripcionModel.init().catch(() => {
     console.log("⚠️ La BD conectará en la primera petición...");
   });
 } catch (e) {
   console.log("Error inicializando contexto de BD");
 }
 
-export const handler = serverless(app, {
-  basePath: '/.netlify/functions/api'
-});
+export const handler = serverless(app);
