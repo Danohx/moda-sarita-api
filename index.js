17f3a1d646a9b41f40bc6f56bf60cc78f463daaa
Danohx <dt094250@gmail.com>
2026-04-05 10:14:53 -0600

    Corrección en Invocación de Funcion para Vercel

--- index.js
+++ index.js
@@ -7,16 +7,15 @@ const PORT = process.env.PORT || 3000;
 
 const startServer = async () => {
   try {
-    // 1. Primero aseguramos la Base de Datos
     await SuscripcionModel.init();
     console.log("✅ Base de Datos conectada y lista");
 
-    // 2. Ahora sí, giramos la llave
     app.listen(PORT, () => {
       console.log(`🚀 Servidor corriendo en http://localhost:${PORT}`);
     });
   } catch (error) {
     console.error("❌ Error fatal al iniciar el servidor:", error);
+    process.exit(1);
   }
 };
 
